05c97f22511415d2dcf3efbf64655d0a8becdccf
Julia McNeill <juliammcneill@gmail.com>
2020-05-19 10:04:45 -0400

    Complete nonrepeatedCharacter

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,18 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+var firstNonRepeatedCharacter = function (string) {
+  // Input: a string of characters
+  // for the character at each index, in order
+  for (var i = 0; i < string.length; i++) {
+    // check if that character exists more than once in the string by finding if it is at another location
+    if (string.indexOf(string[i]) == i && string.indexOf(string[i], i + 1) == -1) {
+      // if not, return that character
+      return string[i];
+    }
+  }
+  // Output: a string of the first non-repeated character
 };
+
+firstNonRepeatedCharacter('ABA'); // 'B'
+firstNonRepeatedCharacter('AACBDB'); // 'C'
