c2e137e1080c06a54f7058359fb2801687f171de
Julia McNeill <juliammcneill@gmail.com>
2020-05-21 09:55:27 -0400

    Complete characterFrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -36,6 +36,42 @@
  */
 
 
-var characterFrequency = function(string) {
-  return result;
+var characterFrequency = function (string) {
+  // Input: a string
+  // creates tracking object
+  var tracking = {};
+
+  // for character in string
+  for (var character of string) {
+    if (tracking[character]) {
+      // if in tracking object, increment count
+      tracking[character] += 1;
+    } else {
+      // if not in tracking object, add to tracking object
+      tracking[character] = 1;
+    }
+  }
+
+  // convert object to array
+  var result = Object.entries(tracking);
+  // sort array, first by number in index 1, then by name in index 0
+  var sorted = result.sort(function (a, b) {
+    if (a[1] < b[1]) return 1;
+    if (a[1] > b[1]) return -1;
+    if (a[0] > b[0]) return 1;
+    if (a[0] < b[0]) return -1;
+  });
+
+  // return result;
+  return sorted;
+  // Output: an array with a smaller array inside with each letter and its frequency, in the specified order 
 };
+
+characterFrequency('miaaiaaippi');
+
+// [
+//   ['a', 4],
+//   ['i', 4],
+//   ['p', 2],
+//   ['m', 1]
+//  ]
