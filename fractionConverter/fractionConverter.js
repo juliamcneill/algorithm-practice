783540a856de73d8360fd73449b0101a8447a9d7
Julia McNeill <juliammcneill@gmail.com>
2020-06-02 09:53:02 -0400

    Complete fractionConverter pseudocode

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,17 +1,23 @@
 /**
- * Write a function that takes a number as its argument and 
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
+ *
  * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
+ *
  * Example: toFraction(3.0) === '3/1'
- * 
+ *
  * Example: toFraction(2.5) === '5/2'
  *
  */
 
-var toFraction = function(number) {
-  // Your code here
+var toFraction = function (number) {
+  // Input: an integer with a decimal place
+  // put decimal over the number of digits in it
+  // find greatest common divisor of numerator and denominator by
+  // divide numerator and denominator by their greatest common divisor
+  // multiple whole number times denominator and add numerator
+  // put this result back over the denominator
+  // Output: a string containing that integer as a fraction
 };
