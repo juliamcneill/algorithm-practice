2c2877b4ea6cedef28909127ab2a493c0d2b567a
Julia McNeill <juliammcneill@gmail.com>
2020-06-06 09:37:48 -0400

    Complete allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -7,11 +7,51 @@
  */
 
 /**
-  * example usage:
-  * var anagrams = allAnagrams('abc');
-  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example usage:
+ * var anagrams = allAnagrams('abc');
+ * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
-var allAnagrams = function(string) {
-  // Your code here.
+var allAnagrams = function (string) {
+  // Input: a string
+  // create an empty tracker object to hold all combinations
+  var tracker = {};
+  // create an empty results array to hold all combinations
+  var results = [];
+  // create an empty temp string to build out the anagrams
+  var temp = "";
+
+  // recursively create the anagrams
+  function buildAnagrams(temp, string) {
+    // in each loop, iterate through all letters
+    for (let i = 0; i < string.length; i++) {
+      // if the letter is not already in the temp string, add it
+      if (temp.indexOf(string[i]) === -1) {
+        temp += string[i];
+        // once a temp string reaches the length of the input string
+        if (temp.length === string.length) {
+          // check if the temp string already exists in the results object
+          if (tracker[temp]) {
+            return;
+            // if not, add it to the results object and results array
+          } else {
+            tracker[temp] = true;
+            results.push(temp);
+          }
+        }
+        // and continue the recursive loop without the current letter
+        buildAnagrams(temp, string);
+        // remove the letter that you just added to try again with a new letter
+        temp = temp.slice(0, -1);
+      }
+    }
+  }
+
+  // trigger the first function iteration
+  buildAnagrams(temp, string);
+
+  return results;
+  // Output: an array with all anagrams of that string
 };
+
+allAnagrams("abc"); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
