d36e990932cb15589a1884566c0d3d524ec669e4
Julia McNeill <juliammcneill@gmail.com>
2020-06-17 09:45:56 -0400

    Complete fractionConverter

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -14,10 +14,46 @@
 
 var toFraction = function (number) {
   // Input: an integer with a decimal place
-  // put decimal over the number of digits in it
-  // find greatest common divisor of numerator and denominator by
-  // divide numerator and denominator by their greatest common divisor
-  // multiple whole number times denominator and add numerator
-  // put this result back over the denominator
+
+  // stringify the integer
+  var stringified = number.toString();
+
+  // if the string doesn't have a decimal, return it over 1
+  if (stringified.indexOf(".") === -1) {
+    return stringified + "/1";
+  }
+
+  // split the string into whole and decimal parts
+  var split = stringified.split(".");
+
+  // calculate the unsimplified numerator and denominator of the decimal part
+  var decimalNum = split[1];
+  var decimalDenom = 10 ** split[1].length;
+
+  // a helper function that uses Euclid's algorithm to calculate the greatest common divisor
+  // and then returns the simplified fraction
+  function reduce(num, denom) {
+    var gcd = function gcd(num, denom) {
+      return denom ? gcd(denom, num % denom) : num;
+    };
+    gcd = gcd(num, denom);
+    // divide numerator and denominator by their greatest common divisor
+    return [num / gcd, denom / gcd];
+  }
+
+  // call the helper function
+  var simplifiedParts = reduce(decimalNum, decimalDenom);
+
+  // multiple the whole number with the denominator and add into the numerator,
+  // and then return the final string
+  return (
+    split[0] * simplifiedParts[1] +
+    simplifiedParts[0] +
+    "/" +
+    simplifiedParts[1]
+  );
+
   // Output: a string containing that integer as a fraction
 };
+
+toFraction(3.5); // "7/2"
