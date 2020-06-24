920f288ebc6d38f7ba768002b7e1f1b982ed65b3
Julia McNeill <juliammcneill@gmail.com>
2020-06-24 16:09:52 -0400

    Complete quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -10,6 +10,37 @@
  *    Combines the two arrays and the pivot into a sorted array.
  */
 
+var quicksort = function (array) {
+  // Input: unsorted array
 
-var quicksort = function(array) {
+  var test = {};
+  // check if array can be sorted based on whether it includes unique values
+  // if it cannot, return the array as it is
+  for (let i = 0; i < array.length; i++) {
+    test[array[i]] = true;
+  }
+  if (Object.keys(test).length <= 1) {
+    return array;
+  }
+
+  // assign a random value in the array to be the pivot
+  var pivot = array[Math.floor(Math.random() * array.length)];
+  let higher = [];
+  let lower = [];
+  // for each value in the array
+  for (let i = 0; i < array.length; i++) {
+    // sort that value into the larger or smaller array
+    if (array[i] >= pivot) {
+      higher.push(array[i]);
+    } else {
+      lower.push(array[i]);
+    }
+  }
+
+  // returning the result of this to the next function up in the
+  // recursive stack, or to the text
+  return splitArray(lower).concat(splitArray(higher));
+  // Output: sorted array
 };
+
+quicksort([4, 1, 8, 4, 9, 10, 1, 4]);
