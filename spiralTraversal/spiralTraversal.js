9d682653d7eac34e5511d53847513c4773ceca79
Julia McNeill <juliammcneill@gmail.com>
2020-07-13 09:30:28 -0400

    Complete spiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -14,7 +14,55 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix) {
+var spiralTraversal = function (matrix) {
+  // create empty array
+  var results = [];
 
-  // TODO: Implement me!
+  // set minimum and maximum locations for each edge of the matrix
+  var startRowIndex = 0;
+  var endRowIndex = matrix.length - 1;
+  var startColIndex = 0;
+  var endColIndex = matrix[0].length - 1;
+
+  while (startRowIndex <= endRowIndex && startColIndex <= endColIndex) {
+    // add entire first row
+    for (var i = startColIndex; i <= endColIndex; i++) {
+      results.push(matrix[startRowIndex][i]);
+    }
+    // reset minimum row location
+    startRowIndex++;
+
+    // add entire last column from second row onwards
+    for (var j = startRowIndex; j <= endRowIndex; j++) {
+      results.push(matrix[j][endColIndex]);
+    }
+    // reset maximum column location
+    endColIndex--;
+
+    // add entire last row backwards, from second to last integer onwards
+    if (startRowIndex <= endRowIndex) {
+      for (var k = endColIndex; k >= startColIndex; k--) {
+        results.push(matrix[endRowIndex][k]);
+      }
+      // reset maximum row location
+      endRowIndex--;
+    }
+
+    // add entire first column, from second to last row to second row
+    if (startColIndex <= endColIndex) {
+      for (var m = endRowIndex; m >= startRowIndex; m--) {
+        results.push(matrix[m][startColIndex]);
+      }
+      // reset minimum column location
+      startColIndex++;
+    }
+  }
+
+  return results;
 };
+
+spiralTraversal([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]); // [1, 2, 3, 6, 9, 8, 7, 4, 5]
