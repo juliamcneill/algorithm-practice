7a0ee916b2b77bb6c221755a569346628a0b8183
Julia McNeill <juliammcneill@gmail.com>
2020-09-24 01:20:55 -0400

    Add fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,22 @@
+// Write a program that outputs the string representation of numbers from 1 to n.
+
+// But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+// For numbers which are multiples of both three and five output “FizzBuzz”.
+
+var fizzBuzz = function (n) {
+  var result = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push('' + i);
+    }
+  }
+
+  return result;
+};
