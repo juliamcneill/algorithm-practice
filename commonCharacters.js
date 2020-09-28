962ea4f120b3b63c9525536ef8a90e1434c1e92d
Julia McNeill <juliammcneill@gmail.com>
2020-09-28 10:14:59 -0400

    Fix findCommonCharacters

--- commonCharacters.js
+++ commonCharacters.js
@@ -1,48 +1,31 @@
-/**
- * Write a function `f(a, b)` which takes two strings as arguments and returns a
- * string containing the characters found in both strings (without duplication), in the
- * order that they appeared in `a`. Remember to skip spaces and characters you
- * have already encountered!
- *
- * Example: commonCharacters('acexivou', 'aegihobu')
- * Returns: 'aeiou'
- *
- * Extra credit: Extend your function to handle more than two input strings.
- */
+// Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).
+// For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
 
-// Assumption: Uppercase and Lowercase letters are handled as different characters.
-var commonCharacters = function (string1, string2) {
-  // Input: two strings of characters
+// You may return the answer in any order.
 
-  // Create tracker, results string, and missing variable
-  var tracker = {};
-  var results = "";
-  let missing;
+var commonChars = function (array) {
+  let results = [];
+  let tracker = {};
 
-  // Iterate through string1
-  for (let i = 0; i < string1.length; i++) {
-    // Check if character is found in results object
-    if (tracker[string1[i]] !== true) {
-      missing = false;
-      // If not, check if found in all other strings, iterating through arguments and using indexOf
-      for (let j = 1; j < arguments.length; j++) {
-        if (arguments[j].indexOf(string1[i]) === -1) {
-          missing = true;
-          continue;
-        }
+  for (let i = 0; i < array[0].length; i++) {
+    let missing = false;
+
+    if (tracker[array[0][i]] === undefined) {
+      tracker[array[0][i]] = 1;
+    } else {
+      tracker[array[0][i]] = tracker[array[0][i]] + 1;
+    }
+
+    for (let j = 1; j < array.length; j++) {
+      if (tracker[array[0][i]] > array[j].split(array[0][i]).length - 1) {
+        missing = true;
       }
-      // If character shows up in all other strings, add to tracker and results string
-      if (missing === false) {
-        tracker[string1[i]] = true;
-        results += string1[i];
-      }
+    }
+
+    if (missing === false) {
+      results.push(array[0][i]);
     }
   }
 
-  // Return results string
   return results;
-  // Output: a string containing the common characters
 };
-
-commonCharacters("acexivou", "aegihobu"); // 'aeiou'
-commonCharacters("Hello, my name is Julia!", "My name is Julia!"); // 'el mynaisJu!'
